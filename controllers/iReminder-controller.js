da86f0fd28e970c94b97fcd367e6d44bc6dda2da
Rashotech <rashotech@gmail.com>
2020-12-08 16:16:11 +0100

    Ireminder API minor fix

--- controllers/iReminder-controller.js
+++ controllers/iReminder-controller.js
@@ -13,11 +13,14 @@ const createReminder = (req, res, next) => {
 // edit a reminder
 const editReminder = (req, res, next) => {
     const { title, message, timeCompleted } = req.body;
-    connection.query(`UPDATE ireminder SET title = '${title}', message = '${message}', timeCompleted = '${timeCompleted}' where id = ${req.params.id}`, (err, response) => {
-        if (err) return res.status(422).json({message : err.sqlMessage})
-        else if (response.affectedRows === 0) return res.status(404).json({message : 'No reminder with the provided id'}) 
-        res.status(200).json({message : 'Reminder edited successfully'})
-    })
+    connection.query(`select * from ireminder where id = ${req.params.id}`, (err, response) => {
+        if (err) return res.status(422).json({message : err.sqlMessage}) 
+        else if (response.length === 0) return res.status(404).json({message : 'Could not find a reminder with the provided id'})
+        connection.query(`UPDATE ireminder SET title = '${title}', message = '${message}', timeCompleted = '${timeCompleted}' where id = ${req.params.id}`, (err, response) => {
+            if (err) return res.status(422).json({message : err.sqlMessage})
+            res.status(200).json({message : 'Reminder edited successfully'})
+        })
+    }); 
 };
 
 // get all reminders
@@ -42,11 +45,14 @@ const getReminder = (req, res, next) => {
 
 // delete a reminder
 const deleteReminder = (req, res, next) => {
-    const sql = `delete from ireminder where id = ${req.params.id}`
-    connection.query(sql, (err, response) => {
+    connection.query(`select * from ireminder where id = ${req.params.id}`, (err, response) => {
         if (err) return res.status(422).json({message : err.sqlMessage}) 
-        else if (response.affectedRows === 0) return res.status(404).json({message : 'No reminder with the provided id'}) 
-        res.status(200).json({message : "Reminder deleted successfully"})
+        else if (response.length === 0) return res.status(404).json({message : 'Could not find a reminder with the provided id'})
+        const sql = `delete from ireminder where id = ${req.params.id}`
+        connection.query(sql, (err, response) => {
+            if (err) return res.status(422).json({message : err.sqlMessage}) 
+            res.status(200).json({message : "Reminder deleted successfully"})
+        }); 
     });
 };
 
@@ -55,4 +61,3 @@ exports.editReminder = editReminder;
 exports.getAllReminders = getAllReminders;
 exports.getReminder = getReminder;
 exports.deleteReminder = deleteReminder;
-
--- routes/iReminder-route.js
+++ routes/iReminder-route.js
@@ -1,9 +1,9 @@
 // Required files
 const express = require('express');
-const { reminderValidationRules, validate } = require('../models/validator')
+const { reminderValidationRules, validate } = require('../models/validator');
 
 // Required Controller
-iReminderControllers = require('../controllers/iReminder-controller');
+const iReminderControllers = require('../controllers/iReminder-controller');
 
 
 const router = express.Router();
@@ -12,11 +12,11 @@ router.post('/add', reminderValidationRules(), validate, iReminderControllers.cr
 
 router.put('/edit/:id', reminderValidationRules(), validate, iReminderControllers.editReminder);
 
-router.get('/reminders', iReminderControllers.getAllReminders);
+router.get('/', iReminderControllers.getAllReminders);
 
 router.get('/reminder/:id', iReminderControllers.getReminder);
 
-router.delete('/reminder/:id', iReminderControllers.deleteReminder);
+router.delete('/delete/:id', iReminderControllers.deleteReminder);
 
 
 module.exports = router;
