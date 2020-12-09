f41c185707a4de56fc2c61468950c72da6da3f23
Rashotech <rashotech@gmail.com>
2020-12-09 09:31:02 +0100

    Contact API

---
+++ controllers/contact-controller.js
@@ -0,0 +1,19 @@
+const connection = require('../models/db');
+const sendMail = require('../util/mail');
+
+const contact = (req, res, next) => {
+    const { customerName, customerEmail, customerSubject, customerMessage} = req.body;
+    const attendedToBy = "rashotech@gmail.com"
+    sendMail(customerName, customerEmail, customerSubject, customerMessage, attendedToBy, function(err, info) {
+        if (err) {
+            res.status(500).json({ message: 'Internal Error' });
+        } else {
+            const sql = `insert into contact_us (customerName, customerEmail, customerSubject, customerMessage, attendedToBy) values ('${customerName}', '${customerEmail}', '${customerSubject}', '${customerMessage}', '${attendedToBy}')`;
+            connection.query(sql, (err, response) => {
+                if (err) return res.status(422).json({message : err.sqlMessage}) 
+        }); 
+                res.status(201).json({ message: 'Email sent!!!' });
+        }});
+};
+
+exports.contact = contact;
--- routes/contact-route.js
+++ routes/contact-route.js
@@ -1,10 +1,13 @@
 // Required files
 const express = require('express');
+const { contactValidationRules, validate } = require('../models/validator');
 
 // Required Controllers
-
+const contactController = require('../controllers/contact-controller')
 
 const router = express.Router();
+
+router.post('/', contactValidationRules(), validate, contactController.contact)
 
 
 module.exports = router;
