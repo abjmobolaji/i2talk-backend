4adc5f59c322f9f23c3ceb40e6294cf9e3aef99c
Rashotech <rashotech@gmail.com>
2021-01-17 11:58:55 +0100

    chat functionalities

--- controllers/chat-controllers.js
+++ controllers/chat-controllers.js
@@ -5,7 +5,7 @@ const getAllCurrentChats = (req, res, next) => {
     const sql = `SELECT * FROM chats WHERE sender = '${req.params.id}' OR receiver = '${req.params.id}' ORDER BY updatedAt DESC`
     connection.query(sql, (err, response) => {
         if (err) return res.status(422).json({message : err.sqlMessage}) 
-        else if (response.length === 0) return res.status(404).json({message : 'Could not find a chat room with the provided id'})
+        // else if (response.length === 0) return res.status(404).json({message : 'Could not find a chat room with the provided id'})
         res.status(200).json({data : response})
     }); 
 };
@@ -15,7 +15,7 @@ const getChatMessages = (req, res, next) => {
     const sql = `SELECT * FROM chat_messages WHERE chatID = '${req.params.id}' AND scheduled = '0' ORDER BY timeSent ASC`
     connection.query(sql, (err, response) => {
         if (err) return res.status(422).json({message : err.sqlMessage}) 
-        else if (response.length === 0) return res.status(404).json({message : 'Could not find any chats with the provided chat id'})
+        // else if (response.length === 0) return res.status(404).json({message : 'Could not find any chats with the provided chat id'})
         res.status(200).json({data : response});
     }); 
 };
